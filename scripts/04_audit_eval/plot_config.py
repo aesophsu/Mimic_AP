256d8527cc42f57becc8c3d0b4d85eb52dec94f5
aesophsu <41796611+aesophsu@users.noreply.github.com>
2026-01-25 13:20:35 +0800

    Create plot_config.py

---
+++ scripts/04_audit_eval/plot_config.py
@@ -0,0 +1,16 @@
+class PlotConfig:
+    FIG_DPI = 300
+    SAVE_DPI = 600
+
+    FOREST_FIGSIZE = (9, 11)
+    NOMOGRAM_FIGSIZE = (14, 12)
+
+    LABEL_FONT = 12
+    TICK_FONT = 10
+    TITLE_FONT = 18
+
+    OR_REF_LINE_COLOR = 'red'
+    OR_POINT_COLOR = 'darkblue'
+    OR_CI_COLOR = 'skyblue'
+
+    LOG_OR_TICKS = [0.1, 0.5, 1, 2, 5, 10]
